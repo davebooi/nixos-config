f65056407f23c319c5a3201d2b1bc119a106d771
davebooi <db@scurr.club>
2026-03-27 12:01:18 +0100

    flake.nix: remove systems

--- flake.nix
+++ flake.nix
@@ -21,9 +21,7 @@
     ...
   } @ inputs: let
     # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "x86_64-linux"
-    ];
+    #systems = [     "x86_64-linux"    ];
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
