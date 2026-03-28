ec65869bd05465df7cd61908014c0dbbaad29616
davebooi <db@scurr.club>
2026-03-28 22:38:06 +0100

    configuration.nix: disabled networking.wireless.enable

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -95,7 +95,7 @@
 
   # hostname
   networking.hostName = "nixos";
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Disabled: conflicts with networkmanager
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
