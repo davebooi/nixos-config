93db4382682836fb31076d7a1222458837b6d7e5
davebooi <db@scurr.club>
2026-03-29 13:09:45 +0200

    laptop/home.nix: remove sops-nix

--- laptop/home.nix
+++ laptop/home.nix
@@ -10,7 +10,7 @@
   
   # You can import other home-manager modules here
   imports = [
-    inputs.sops-nix.homeManagerModules.sops
+    #inputs.sops-nix.homeManagerModules.sops
 
     # If you want to use modules your own flake exports (from modules/home-manager):
     # inputs.self.homeManagerModules.example
