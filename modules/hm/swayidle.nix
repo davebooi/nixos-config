015605ccb9c2185d5d14274e53abf9dcff4c03f1
davebooi <db@scurr.club>
2026-03-30 01:05:54 +0200

    fix: swayidle.nix module

--- modules/hm/swayidle.nix
+++ modules/hm/swayidle.nix
@@ -1,53 +1,57 @@
-services.swayidle =
-let
-  # Lock command
-  lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
-  # TODO: modify "display" function based on your window manager
-  # Sway
-  #display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
-  # Hyprland
-  # display = status: "hyprctl dispatch dpms ${status}";
-  # Niri
-  display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
-in
+{ pkgs, inputs, lib, ... }:
+
 {
-  enable = true;
-  timeouts = [
-    {
-      timeout = 295; # in seconds
-      command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
-    }
-    {
-      timeout = 300;
-      command = lock;
-    }
-    {
-      timeout = 600;
-      command = display "off";
-      resumeCommand = display "on";
-    }
-    {
-      timeout = 1800;
-      command = "${pkgs.systemd}/bin/systemctl suspend";
-    }
-  ];
-  events = [
-    {
-      event = "before-sleep";
-      # adding duplicated entries for the same event may not work
-      command = (display "off") + "; " + lock;
-    }
-    {
-      event = "after-resume";
-      command = display "on";
-    }
-    {
-      event = "lock";
-      command = (display "off") + "; " + lock;
-    }
-    {
-      event = "unlock";
-      command = display "on";
-    }
-  ];
+  services.swayidle =
+  let
+    # Lock command
+    lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+    # TODO: modify "display" function based on your window manager
+    # Sway
+    #display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
+    # Hyprland
+    # display = status: "hyprctl dispatch dpms ${status}";
+    # Niri
+    display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+  in
+  {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 295; # in seconds
+        command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+      }
+      {
+        timeout = 300;
+        command = lock;
+      }
+      {
+        timeout = 600;
+        command = display "off";
+        resumeCommand = display "on";
+      }
+      {
+        timeout = 1800;
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+      }
+    ];
+    events = [
+      {
+        event = "before-sleep";
+        # adding duplicated entries for the same event may not work
+        command = (display "off") + "; " + lock;
+      }
+      {
+        event = "after-resume";
+        command = display "on";
+      }
+      {
+        event = "lock";
+        command = (display "off") + "; " + lock;
+      }
+      {
+        event = "unlock";
+        command = display "on";
+      }
+    ];
+  };
 };
